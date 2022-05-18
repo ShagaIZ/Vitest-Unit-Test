2eb3123e970a1a8197def6456d2512fea41a7773
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2022-05-18 22:29:20 +0500

    Удаление файла второй этап

--- src/checkCharacterCase/checkCharacterCase.ts
+++
@@ -1,9 +0,0 @@
-export function sameCase (a:string, b:string){
-  if(((a.match(/[a-z]/) && b.match(/[a-z]/))||(a.match(/[A-Z]/) && b.match(/[A-Z]/)))){
-    return 1
-  }else if((a.match(/^[a-zA-Z]+$/) && b.match(/^[a-zA-Z]+$/) )){
-    return  0
-  }else{
-    return -1
-  } 
-};
