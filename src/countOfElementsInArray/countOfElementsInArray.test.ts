68c57a2469ee4914a7291884c6a838986cff836d
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2022-06-20 22:36:24 +0500

    Добавил новую функции и тесты к ней

---
+++ src/countOfElementsInArray/countOfElementsInArray.test.ts
@@ -0,0 +1,12 @@
+import { assert, describe, expect, test, it } from 'vitest'
+import { countPositivesSumNegatives }  from  "./countOfElementsInArrayFunc";
+import { arrays  } from "./countOfElementsInArrayData";
+
+describe.only('Базовые тесты', ()=>{
+
+    test('Числа(+ и -) в массиве -> возвращается новый массив: элемент[0]=кол-во положительных чисел, элемент[1]=сумма отрицательных чисел',()=>{
+        expect(countPositivesSumNegatives(arrays.firstInput)).toBeTypeOf('object');
+        expect(countPositivesSumNegatives(arrays.firstInput)).toHaveLength(2);
+        expect(countPositivesSumNegatives(arrays.firstInput)).toEqual(arrays.firstOutput);
+    });
+})
