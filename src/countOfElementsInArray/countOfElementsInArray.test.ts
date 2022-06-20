68c3587a6a005c5a73928fef24cafdeef0d8a373
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2022-06-20 22:43:28 +0500

    Поправил тест

--- src/countOfElementsInArray/countOfElementsInArray.test.ts
+++ src/countOfElementsInArray/countOfElementsInArray.test.ts
@@ -2,7 +2,7 @@ import { assert, describe, expect, test, it } from 'vitest'
 import { countPositivesSumNegatives }  from  "./countOfElementsInArrayFunc";
 import { arrays  } from "./countOfElementsInArrayData";
 
-describe.only('Базовые тесты', ()=>{
+describe('Базовые тесты', ()=>{
 
     test('Числа(+ и -) в массиве -> возвращается новый массив: элемент[0]=кол-во положительных чисел, элемент[1]=сумма отрицательных чисел',()=>{
         expect(countPositivesSumNegatives(arrays.firstInput)).toBeTypeOf('object');
