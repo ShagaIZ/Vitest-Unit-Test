276e103cffbd87fc9c88297853f28f06da6c5685
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2022-08-02 21:06:15 +0500

    Добавил enum перечисления для тестов SquareEveryDigit

---
+++ src/SqureEveryDigit/Data.ts
@@ -0,0 +1,11 @@
+export const enum InputNumbers  {
+    FourOne = 1111,
+    FourTwo = 2222,
+    FourThree = 3333,
+}
+
+export const enum OutputNumbers  {
+    FourOne = 1111,
+    FourTwo = 4444,
+    FourThree = 9999
+}
